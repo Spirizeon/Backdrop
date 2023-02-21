d6d660c6b17d6cf75d9d535ea4da08e6ee3c90bf
Spirizeon <123345456+Spirizeon@users.noreply.github.com>
2023-02-21 19:27:42 +0530

    Add files via upload

---
+++ batchinstaller/backdrops.bat
@@ -0,0 +1,6 @@
+winget install --id Git.Git -e --source winget
+cd %HOMEPATH%
+cd Pictures
+git clone https://github.com/Spirizeon/backdrops.git
+echo "ALL DONE!"
+start backdrops
---
+++ batchinstaller/backdrops.sh
@@ -0,0 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
+cd $home
+git clone https://github.com/Spirizeon/backdrops.git
+echo "ALL DONE!"
